8025d3c5715e305f61ab482b77b55b77f1d9a06f
v sotnikov <sotnikov@rosplatforma.ru>
2024-02-12 11:40:37 +0300

    12022024commit

---
+++ src/ServiceLocator/Solution.java
@@ -0,0 +1,25 @@
+package ServiceLocator;
+
+import ServiceLocator.service.Service;
+
+/*
+Service Locator
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Service service = ServiceLocator.getService("EJBService");
+        service.execute();
+        System.out.println();
+        service = ServiceLocator.getService("JMSService");
+        service.execute();
+        System.out.println();
+        service = ServiceLocator.getService("EJBService");
+        service.execute();
+        System.out.println();
+        service = ServiceLocator.getService("JMSService");
+        service.execute();
+
+    }
+
+}
---
+++ src/ServiceLocator/service/impl/JMSServiceImpl.java
@@ -0,0 +1,17 @@
+package ServiceLocator.service.impl;
+
+import ServiceLocator.service.Service;
+
+public class JMSServiceImpl implements Service {
+
+    @Override
+    public void execute() {
+        System.out.println("Executing JMSService");
+    }
+
+    @Override
+    public String getName() {
+        return "JMSService";
+    }
+
+}
